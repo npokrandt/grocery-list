ee4c69d225fcecbe70a8723976a33ab351a665ad
npokrandt <npokrandt919@gmail.com>
2024-03-11 16:54:47 -0500

    model planning, db creation, and new npm commands

---
+++ models/index.js
@@ -0,0 +1,25 @@
+//ideas
+
+//aisleOrder
+//name
+//grocId
+
+//groceryItem
+//id int
+//name string
+//aisleId
+//price float?
+//needMore bool
+
+//many to many?
+
+//recipe
+//id int
+//name string
+//ingredients [string or id]
+
+//meal
+
+//id
+//name
+//dishes [recipe or groceryItem id]
